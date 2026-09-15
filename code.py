98d38429f3e7870967cbbf96f8de478d00ede2ad
Jaden Scott <64746@thedps.org>
2026-09-15 14:07:27 -0400

    Update code.py

--- code.py
+++ code.py
@@ -13,6 +13,10 @@ st.markdown("""
             background-color: Cyan;
             color: Black;
         }
+
+        .stAppToolbar {
+            visibility: hidden;
+        }
     </style>
 """, unsafe_allow_html=True)
 
